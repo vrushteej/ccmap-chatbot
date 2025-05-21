7ae4be3553ca7bb178744f536cd57ce6c30322a9
vrushteej <149369326+vrushteej@users.noreply.github.com>
2025-05-21 16:31:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,5 @@ def chat():
     return jsonify({"response": bot_response})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
